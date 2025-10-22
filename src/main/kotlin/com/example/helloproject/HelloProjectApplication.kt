55021fb1d04d0eacf5dd970b7af8caf9960fc3a9
julskopova <julskopova@gmail.com>
2025-10-22 18:00:36 +0200

    create-github-project

---
+++ src/main/kotlin/com/example/helloproject/HelloProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.helloproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloProjectApplication>(*args)
+}
